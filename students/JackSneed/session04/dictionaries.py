689c8ea37cbf7126b282ea755e26f2db53f5af83
Jack <sneed.jack@gmail.com>
2015-03-26 16:00:35 -0700

    Add dictionaries.py

---
+++ students/JackSneed/session04/dictionaries.py
@@ -0,0 +1,41 @@
+def getting_sneaky():
+    """Can I sneak a list key into a dictionary via tuple?"""
+
+    d = {}
+    d[([1,2,3])] = u"hope this works!"
+
+#getting_sneaky()
+# NO! After testing, returns "TypeError: unhashable type: 'list'
+
+
+def find_key():
+    """What happens when I look for a key that isn't there?"""
+
+    d = {u'here':1, u'i':2, u'am':3}
+    d.has_key(2)
+    print(d.has_key(2))
+
+
+find_key()
+
+
+def update_dict():
+    """Can I update a dictionary?"""
+
+    d = {u'here':1, u'i':2, u'am':3}
+    d.update(oh =  1, boy = 2)
+    print(d)
+
+
+update_dict()
+
+
+def set_trippin():
+    """Can I create a new set from two existing sets?"""
+
+    s1 = set([1,2,3])
+    s2 = set([4,5,6])
+    s3 = s1.union(s2)
+    print(s3)
+
+set_trippin()
