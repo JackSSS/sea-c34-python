726d0ede47231a325aee675829d441b73e51aba8
Jack <sneed.jack@gmail.com>
2015-03-29 14:56:13 -0700

    Add initial mailroom_madness.py file.

---
+++ students/JackSneed/session04/mailroom_madness.py
@@ -0,0 +1,5 @@
+Using all you’ve learned so far, complete your mailroom program according to the pseudocode and flow chart you created last session.
+use dicts where appropriate
+see if you can use a dict to switch between the users selections
+Try to use a dict and the .format() method to do the letter as one big template – rather than building up a big string in parts.
+For extra fun, see if you can use a file to preserve the donation list and changes made to it while the program is running.
