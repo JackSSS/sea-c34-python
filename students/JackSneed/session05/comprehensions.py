7ca195a309c6d8fa2c95e11540cde43b7300f1f9
Jack <sneed.jack@gmail.com>
2015-04-01 16:40:23 -0700

    Add comprehensions.py

---
+++ students/JackSneed/session05/comprehensions.py
@@ -0,0 +1,44 @@
+def comprehend():
+    """Can a replicate a traditional for loop with comprehension?"""
+
+
+    numbers = range(10)
+
+    hard_copy = []
+
+    for i in numbers:
+        hard_copy.append(i)
+        print(hard_copy)
+
+    hard_copy2 = [i for i in numbers]
+    print(hard_copy2)
+
+comprehend()
+
+
+
+
+def make_a_float():
+    """Using comprehension, can I zip a dict of iinteger keys with there float
+    value?
+
+    """
+
+    numbers = range(0, 16)
+    float_value = dict(zip(numbers, [float(f_number) for f_number in numbers]))
+    print float_value
+
+
+make_a_float()
+
+
+
+
+def math_stuff():
+    """Can I get the value of key * 11?"""
+
+
+    equation = {i: "%i" % i*2 for i in range(1, 6)}
+    print(equation)
+
+math_stuff()
