0aefc1a874bdd6d63c93b15593d7e984fbe069c6
Jack <sneed.jack@gmail.com>
2015-04-03 23:34:14 -0700

    Add dict-comp.py with problems 1-3

---
+++ students/JackSneed/session05/dict_comp.py
@@ -0,0 +1,29 @@
+def problem1():
+
+    food_prefs = {u"name": u"Jack",
+                  u"city": u"Richmond",
+                  u"cake": u"chocolate",
+                  u"fruit": u"plum",
+                  u"salad": u"arugula",
+                  u"pasta": u"linguini"}
+
+    print("{name} is from {city}, and he likes {cake} cake, {fruit} fruit, \
+          {salad} salad, and {pasta} pasta".format(**food_prefs))
+
+problem1()
+
+
+def problem2():
+
+    numbers = range(0, 16)
+    result = dict(zip(numbers, [hex(hexadecimal) for hexadecimal in numbers]))
+    print(result)
+
+problem2()
+
+
+def problem3():
+
+    print({numbers : hex(numbers) for numbers in range(0, 16)})
+
+problem3()
