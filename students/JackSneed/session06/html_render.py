7e3d46466ade2a1d22dc05277fb40833b7bdbb32
Jack <sneed.jack@gmail.com>
2015-04-12 17:20:13 -0700

    Finish step 2

--- students/JackSneed/session06/html_render.py
+++ students/JackSneed/session06/html_render.py
@@ -7,15 +7,29 @@ class Element(object):
     indent = u"    "
 
     def __init__(self, content=None):
-        self.content = self.indent + str(self.content) if content else ""
+        self.el = [str(content)] if content else []
 
     def append(self, string):
-        self.content += (self.indent + string)
+        self.el.append(string)
 
     def render(self, file_out, ind=""):
-        """Render the tag and strings in content."""
-        output = (self.indent + "<" + self.tag + ">\n"
-                     + self.indent + self.content + "\n"
-                     + self.indent + "</" + self.tag + ">")
-        file_out.write(output)
 
+        file_out.write(ind + "<" + self.tag + ">\n")
+
+        for i in self.el:
+            try:
+                i.render(file_out, self.indent + ind)
+            except AttributeError:
+                file_out.write(self.indent + ind + unicode(i) + "\n")
+
+        file_out.write(ind + "</" + self.tag + ">\n")
+
+class Html(Element):
+    header = u"<!DOCTYPE html>\n"
+    tag = u"html"
+
+class Body(Element):
+    tag = u"body"
+
+class P(Element):
+    tag = u"p"
