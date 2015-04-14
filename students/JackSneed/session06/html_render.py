1bf07a7875d01a827dace1daa89267ca521cf4ee
Jack <sneed.jack@gmail.com>
2015-04-13 21:50:18 -0700

    Fix style errors

--- students/JackSneed/session06/html_render.py
+++ students/JackSneed/session06/html_render.py
@@ -27,6 +27,7 @@ class Element(object):
 
         file_out.write(ind + "</" + self.tag + ">\n")
 
+
 class OneLineTag(Element):
 
     def render(self, file_out, ind=u""):
@@ -40,6 +41,7 @@ class OneLineTag(Element):
 
         file_out.write("</" + self.tag + ">\n")
 
+
 class Html(Element):
     header = u"<!DOCTYPE html>\n"
     tag = u"html"
@@ -49,24 +51,31 @@ class Html(Element):
         file_out.write('<!DOCTYPE html>\n')
         Element.render(self, file_out, indent)
 
+
 class SelfClosingTag(Element):
     def render(self, file_out, ind=u""):
         file_out.write(ind + "<" + self.tag + self.attr + "> \n")
 
+
 class Head(Element):
     tag = u"head"
+
 
 class Meta(SelfClosingTag):
     tag = u"meta"
 
+
 class Body(Element):
     tag = u"body"
+
 
 class Title(OneLineTag):
     tag = u"title"
 
+
 class P(Element):
     tag = u"p"
+
 
 class Hr(SelfClosingTag):
     tag = u"hr"
@@ -74,6 +83,7 @@ class Hr(SelfClosingTag):
 
 class Br(SelfClosingTag):
     tag = u"br"
+
 
 class A(OneLineTag):
     tag = u'a'
